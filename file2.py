f005bb8cea99c2dca32f93a9f843c1cbcc43dc08
vamsi50004 <2101050004@kluniversity.i>
2022-05-05 12:21:51 +0530

    Simple Calculator

---
+++ file2.py
@@ -0,0 +1,18 @@
+a=eval(input("Enter a number:"))
+b=eval(input("Enter b number:"))
+print("1.Addition")
+print("2.Subtraction")
+print("3.Multiplication")
+print("4.Division")
+ch=eval(input("Enter an operation:"))
+if(ch==1:)
+	print("The sum of two numbers is:",a+b)
+elif(ch==2):
+	print("The difference of two umbers is:",a-b);
+elif(ch==3):
+	print("The product of two numbers is:",a*b)
+elseif(ch==4):
+	print("The division of two numbers is:"a/b)
+else:
+	print("invalid choice")
+
